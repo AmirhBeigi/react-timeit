1d7a9c8ca0753ab36f95f704da48a1471d6b3cdb
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2022-01-28 19:20:07 +0330

    feat: setup project :D

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { default as Timeit } from "./timeit";
---
+++ src/timeit.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export interface ButtonProps {
+  label: string;
+}
+
+const Timeit = (props: ButtonProps) => {
+  return <h1>Hello Timeit</h1>;
+};
+
+export default Timeit;
